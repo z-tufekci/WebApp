d588fffcb4a4ed89db7efb3ec06d294cca23a6a7
tufekci <tufekci.z@northeastern.edu>
2021-03-06 15:01:41 -0500

    a4 changes

--- demo/src/main/java/com/example/demo/entity/Authority.java
+++ demo/src/main/java/com/example/demo/entity/Authority.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "authorities") 
+@Table(name = "authorities", catalog = "user_db") 
 public class Authority {
 	@Id
 	@Column(name="username")
