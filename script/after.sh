5e38351bb3626917d01a2e70a9e7a66f65d82959
tufekci <tufekci.z@northeastern.edu>
2021-03-25 16:34:58 -0400

    cw-config

--- script/after.sh
+++ script/after.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-sudo systemctl stop tomcat
+#sudo systemctl stop tomcat
 
-sudo rm -rf /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT
+#sudo rm -rf /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT
 
 sudo chown tomcat:tomcat /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT.war
 
-sudo rm -rf /opt/tomcat/logs/*.log
-sudo rm -rf /opt/tomcat/logs/*.txt
-sudo rm -rf /opt/tomcat/logs/catalina*
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
