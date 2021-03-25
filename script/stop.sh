cdfd6b7f48cd5ae2aab4d5d76cf16e17bd5941d3
tufekci <tufekci.z@northeastern.edu>
2021-03-25 16:53:23 -0400

    up

--- script/stop.sh
+++ script/stop.sh
@@ -3,7 +3,7 @@
 sudo systemctl stop tomcat
 
 sudo rm -rf /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT
-
+sudo rm -rf /opt/cloudwatch-config.json
 sudo rm -rf /opt/tomcat/logs/*.log
 sudo rm -rf /opt/tomcat/logs/*.txt
 sudo rm -rf /opt/tomcat/logs/catalina*
