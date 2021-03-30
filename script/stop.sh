75c1534e8a72aa20841eddab2549bba93e86f3e9
tufekci <tufekci.z@northeastern.edu>
2021-03-30 16:53:32 -0400

    ROOT.war

--- script/stop.sh
+++ script/stop.sh
@@ -3,6 +3,7 @@
 sudo systemctl stop tomcat
 
 sudo rm -rf /opt/tomcat/webapps/ROOT
+sudo rm -rf /opt/tomcat/webapps/ROOT.war
 sudo rm  /opt/cloudwatch-config.json
 sudo rm -rf /opt/tomcat/logs/*.log
 sudo rm -rf /opt/tomcat/logs/*.txt
