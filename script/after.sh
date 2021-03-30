2becff97f11a270a60944f6d9f97969645accb23
tufekci <tufekci.z@northeastern.edu>
2021-03-30 16:47:54 -0400

    ROOT.war

--- script/after.sh
+++ script/after.sh
@@ -4,7 +4,7 @@
 
 #sudo rm -rf /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT
 
-sudo chown tomcat:tomcat /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT.war
+sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
 
