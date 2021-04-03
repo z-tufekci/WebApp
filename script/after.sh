b61f207bbc45a585ca1fe65b2154fd277efdc796
tufekci <tufekci.z@northeastern.edu>
2021-04-03 03:41:26 -0400

    start script

--- script/after.sh
+++ script/after.sh
@@ -4,5 +4,4 @@ sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -o default -s
 
-sudo chmod -R +x /opt/tomcat/
-sudo ./opt/tomcat/bin/catalina.sh run
+sudo bash /opt/tomcat/bin/catalina.sh run
