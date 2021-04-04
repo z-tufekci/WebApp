7f390a0902b6b0a8393347557436cad628a52a0f
tufekci <tufekci.z@northeastern.edu>
2021-04-04 01:54:40 -0400

    script up

--- script/start.sh
+++ script/start.sh
@@ -2,4 +2,4 @@
 
 #sudo systemctl start tomcat
 sudo chmod +x /opt/tomcat/bin/startup.sh
-sudo bash /opt/tomcat/bin/startup.sh run
+sudo bash /opt/tomcat/bin/startup.sh run > /dev/null 2> /dev/null < /dev/null &
