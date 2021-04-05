8382d83c36b1cfeb3224760896c80bab86ee927b
tufekci <tufekci.z@northeastern.edu>
2021-04-05 03:44:18 -0400

    hey

--- script/start.sh
+++ script/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo chmod 755 /opt/tomcat/webapps/ROOT
 sudo rm -r /opt/tomcat/webapps/ROOT
 sudo chmod +x /opt/tomcat/bin/startup.sh
 sudo systemctl restart tomcat > /dev/null 2> /dev/null < /dev/null &
