4acf742fb49ee347090ad1248aa42333b5e96b10
tufekci <tufekci.z@northeastern.edu>
2021-04-11 15:05:36 -0400

    add

--- script/start.sh
+++ script/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /opt/webapps
-test -f pid.file && sudo rm -r pid.file  & > /dev/null 2> /dev/null < /dev/null &
+#test -f pid.file && sudo rm -r pid.file  & > /dev/null 2> /dev/null < /dev/null &
 #sudo chmod +x /opt/tomcat/bin/startup.sh
 
 source /etc/profile & > /dev/null 2> /dev/null < /dev/null &
-java -jar $JAVA_OPTS ROOT.jar & sudo sh -c "echo $!  > pid.file" & > /dev/null 2> /dev/null < /dev/null &
+java -jar $JAVA_OPTS ROOT.jar  & > /dev/null 2> /dev/null < /dev/null &
 
 #sudo chmod 755 /opt/tomcat/webapps/ROOT
 #sudo rm -r /opt/tomcat/webapps/ROOT
--- script/stop.sh
+++ script/stop.sh
@@ -2,7 +2,7 @@
 
 #sudo systemctl stop tomcat
 cd /opt/webapps
-test -f pid.file && kill $(cat pid.file)
+#test -f pid.file && kill $(cat pid.file)
 test -f ROOT.jar && sudo rm -r ROOT.jar
 
 sudo kill -9 `sudo lsof -t -i:8080`
