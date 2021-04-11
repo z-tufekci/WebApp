30b0a87ce3d27408de556e6e657991e060eb2a54
tufekci <tufekci.z@northeastern.edu>
2021-04-11 19:12:39 -0400

    some

--- script/stop.sh
+++ script/stop.sh
@@ -2,7 +2,7 @@
 
 #sudo systemctl stop tomcat
 cd /opt/webapps
-#test -f pid.file && kill $(cat pid.file)
+#test -f pid.file && { kill $(cat pid.file) && wait $(cat pid.file) } 2>/dev/null
 test -f ROOT.jar && sudo rm -r ROOT.jar
 
 test -d /proc/$(sudo lsof -t -i:8080) && { kill $(sudo lsof -t -i:8080) && wait $(sudo lsof -t -i:8080); } 2>/dev/null
