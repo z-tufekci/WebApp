bcbd5447a78e104adbf84550ee5a207554a00e85
tufekci <tufekci.z@northeastern.edu>
2021-04-11 17:41:25 -0400

    add

--- script/stop.sh
+++ script/stop.sh
@@ -2,10 +2,14 @@
 
 #sudo systemctl stop tomcat
 cd /opt/webapps
-#test -f pid.file && kill $(cat pid.file)
+test -f pid.file && kill $(cat pid.file)
 test -f ROOT.jar && sudo rm -r ROOT.jar
 
-sudo kill -9 `sudo lsof -t -i:8080`
+sudo lsof -i:8080 -t | xargs -r sudo kill
 
+#if pids=$(sudo lsof -i:8080 -t); then
+#    sudo kill -9 $pids
+#fi
 
+#sudo kill -9 `sudo lsof -t -i:8080`
 
