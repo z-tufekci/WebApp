eecde0fd9a5c90d7529ef39d6d31d93e52871031
tufekci <tufekci.z@northeastern.edu>
2021-04-11 18:32:01 -0400

    some

--- script/stop.sh
+++ script/stop.sh
@@ -5,5 +5,5 @@ cd /opt/webapps
 #test -f pid.file && kill $(cat pid.file)
 test -f ROOT.jar && sudo rm -r ROOT.jar
 
-test -d /proc/$(sudo lsof -t -i:8080) && sudo kill $(sudo lsof -t -i:8080)
+test -d /proc/$(sudo lsof -t -i:8080) && kill $(sudo lsof -t -i:8080)
 
