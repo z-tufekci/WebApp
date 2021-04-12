43f93435fbf912d4ec0ff20724aeb447548e440f
tufekci <tufekci.z@northeastern.edu>
2021-04-11 23:22:41 -0400

    omrum gecti

--- script/start.sh
+++ script/start.sh
@@ -7,7 +7,7 @@
 #source /etc/profile.d/setenv.sh
 #source /home/ubuntu/.bashrc
 cd /opt/webapps
-source setenv.sh
+source setenv.sh & > /dev/null 2> /dev/null < /dev/null &
 
 java -jar $JAVA_OPTS /opt/webapps/ROOT.jar  & > /dev/null 2> /dev/null < /dev/null &
 
