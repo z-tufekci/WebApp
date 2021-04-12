f79e5060cc9e2aa7da95cb27baa2d4dd4626fe0f
tufekci <tufekci.z@northeastern.edu>
2021-04-11 20:38:30 -0400

    add

--- script/start.sh
+++ script/start.sh
@@ -2,19 +2,23 @@
 
 
 #test -f pid.file && sudo rm -r pid.file  & > /dev/null 2> /dev/null < /dev/null &
-#sudo chmod +x /opt/tomcat/bin/startup.sh
 
-source /etc/profile & > /dev/null 2> /dev/null < /dev/null &
+#source /etc/profile & > /dev/null 2> /dev/null < /dev/null &
+source /etc/profile.d/setenv.sh
 #cd /opt/webapps
 java -jar $JAVA_OPTS /opt/webapps/ROOT.jar  & > /dev/null 2> /dev/null < /dev/null &
 
+
+
+
+
+
+
 #sudo chmod 755 /opt/tomcat/webapps/ROOT
 #sudo rm -r /opt/tomcat/webapps/ROOT
-
-
+#sudo chmod +x /opt/tomcat/bin/startup.sh
 
 #sudo systemctl restart tomcat > /dev/null 2> /dev/null < /dev/null &
-
 
 #sudo bash /opt/tomcat/bin/startup.sh run > /dev/null 2> /dev/null < /dev/null &
 
