ff6914890d8e039b6305f1c60836c347f12f023d
tufekci <tufekci.z@northeastern.edu>
2021-04-12 03:04:44 -0400

    inshallah

--- script/stop.sh
+++ script/stop.sh
@@ -3,7 +3,7 @@
 #sudo systemctl stop tomcat
 cd /opt/webapps
 #test -f pid.file && { kill $(cat pid.file) && wait $(cat pid.file) } 2>/dev/null
-test -f ROOT.jar && sudo rm -r ROOT.jar
+{test -f ROOT.jar && sudo rm -r ROOT.jar} 2>/dev/null
 
-test -d /proc/$(sudo lsof -t -i:8080) && { kill $(sudo lsof -t -i:8080) && wait $(sudo lsof -t -i:8080); } 2>/dev/null
+test -d /proc/$(sudo lsof -t -i:8080) && { kill $(sudo lsof -t -i:8080) && wait $(sudo lsof -t -i:8080); } & > /dev/null 2> /dev/null < /dev/null &
 
